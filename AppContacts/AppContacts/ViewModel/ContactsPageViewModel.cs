5b476959a289dc460ebd455cb41e32930b5a8381
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-06-18 21:36:51 -0500

    configuraciones

---
+++ AppContacts/AppContacts/ViewModel/ContactsPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppContacts.View;
+using AppContacts.Model;
+using AppContacts.Helper;
+
+namespace AppContacts.ViewModel
+{
+   public class ContactsPageViewModel
+    {
+        public ObservableCollection<Grouping<string, Contact>> ContactsLists { get; set; }
+        public Command AddContactCommand { get; set; }
+        public INavigation Navigation { get; set; }
+
+        public ContactsPageViewModel(INavigation navigation)
+        {
+            this.Navigation = navigation;
+            Task.Run(async () => ContactsLists = await App
+                                .DataBase.GetAllGrouped()).Wait();
+            AddContactCommand = new Command(
+                async () => await GoToDetailContact()
+                );
+
+        }
+
+        private async Task GoToDetailContact()
+        {
+            await Navigation.PushAsync(new ContactDetailPage());
+        }
+    }
+}
